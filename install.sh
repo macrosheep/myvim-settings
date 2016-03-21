f28ae58abde0feaa8ef7b28a375a81b7ff6dc7fd
Yang Hongyang <hongyang.yang@easystack.cn>
2016-03-21 07:47:59 -0400

    add hostname to PS1

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cp -r plugin ~/.vim/
 echo "
 source /usr/share/git-core/contrib/completion/git-prompt.sh
 #PS1='[\\u@\\h \\W\$(__git_ps1 \" \\e[1;35m(%s)\\e[m\")]\\\$ '
-PS1='[\\u \\W\$(__git_ps1 \" (%s)\")]\\\$ '
+PS1='[\\u@\\h \\W\$(__git_ps1 \" (%s)\")]\\\$ '
 
 alias uptags='ctags -R; cscope -Rbkq'
 " >> ~/.bashrc
