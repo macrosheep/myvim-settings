510234bead3f30236a59e8ff70d2ea851ce77085
Yang Hongyang <burnef@gmail.com>
2019-07-20 17:26:39 +0800

    add custom bash ps1
    Signed-off-by: Yang Hongyang <burnef@gmail.com>

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp vimrc ~/.vimrc
 
+#PS1=$'\e[1;33m\u256d\e[m \w$(__git_ps1 " \e[1;35m(%s)\e[m")\n\e[1;33m\u2570\e[m Yang \$ '
+
 echo "
 source /usr/share/git-core/contrib/completion/git-prompt.sh
 #PS1='[\\u@\\h \\W\$(__git_ps1 \" \\e[1;35m(%s)\\e[m\")]\\\$ '
